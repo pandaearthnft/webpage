c11161ad8bb55fd6881e9eedf4f24a2353650606
Your Name <you@example.com>
2022-01-17 22:32:13 +0200

    Init commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
